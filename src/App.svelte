c5a939e9a1507dcd8dec030a3dfab73541a0ffb8
amuhleth42 <96829299+amuhleth42@users.noreply.github.com>
2023-08-22 16:02:23 +0200

    Login system work in progress

--- src/App.svelte
+++ src/App.svelte
@@ -3,46 +3,72 @@
 import { AuthClient } from '@dfinity/auth-client';
 import { Actor, HttpAgent } from '@dfinity/agent';
   import { idlFactory } from './declarations/backend';
+import Home from './Home.svelte';
+import { writable } from 'svelte/store';
+import { all } from './global'
+
+let authClient;
+
+$: isLoggedIn = all.loggedIn;
+
+$: console.log("bro? ", isLoggedIn);
+
+let login = async () => {
+  await authClient.login({
+    identityProvider:
+      process.env.DFX_NETWORK === "ic"
+      ? "https://identity.ic0.app/#authorize"
+      : `http://localhost:4943?canisterId=${process.env.CANISTER_ID_INTERNET_IDENTITY}#authorize`,
+    onSuccess: async () => {
+      handleAuthenticated(authClient);
+    },
+  });
+};
+
+let logout = () => {
+  console.log("yo hey");
+
+};
+
 
 const init = async () => {
-  const authClient = await AuthClient.create();
+
+  all.loggedIn = false;
+  //process.env.CANISTER_ID_INTERNET_IDENTITY = 'br5f7-7uaaa-aaaaa-qaaca-cai';
+  process.env.CANISTER_ID_INTERNET_IDENTITY = 'be2us-64aaa-aaaaa-qaabq-cai';
+  console.table(process.env);
+  authClient = await AuthClient.create();
   if (await authClient.isAuthenticated()) {
     handleAuthenticated(authClient);
   }
   
-  let loginButton = document.querySelector('#loginButton') as HTMLButtonElement;
-  loginButton.onclick = async () => {
-    await authClient.login({
-      onSuccess: async () => {
-        handleAuthenticated(authClient);
-      },
-    });
-  };
+
+  /*let logoutButton = document.querySelector('#logoutButton') as HTMLButtonElement;
+    logoutButton.onclick = async () => {
+        await console.log("click");
+    };*/
 };
 
 
-async function handleAuthenticated(authClient: AuthClient) {
-  const identity = await authClient.getIdentity();
 
-  const agent = new HttpAgent({ identity });
-
-  console.log(process.env.CANISTER_ID);
-  const whoami_actor = Actor.createActor<_SERVICE>(idlFactory, {
-    agent,
-    canisterId: process.env.CANISTER_ID as string,
-  });
-  renderLoggedIn(whoami_actor, authClient);
+async function handleAuthenticated(authClient) {
+  const identity = (await authClient.getIdentity());
+  all.loggedIn = true;
+  console.log("auth success!");
 }
+
 
 init();
 
 </script>
 
 <main>
-      <h1>Login test</h1>
-    
-
-     <button id="loginButton">Sign in with Internet Identity</button>
+  {#if !isLoggedIn}
+    <h1>Login test</h1>
+    <button on:click={login} id="loginButton">Sign in with Internet Identity</button>
+  {:else}
+    <Home {authClient}></Home>
+  {/if}
     
     
 </main>
---
+++ src/Home.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+
+    import { AuthClient } from '@dfinity/auth-client';
+    import { all } from './global';
+
+    export let authClient: AuthClient;
+
+    function logout() {
+        console.log("clicked logout");
+        //console.log(authClient);
+
+        //console.log(loggedIn);
+        authClient.logout();
+        all.loggedIn = false;
+        console.log(all.loggedIn);
+    }
+
+</script>
+
+
+<h1>You are inside !</h1>
+
+<button on:click={ logout } id='logoutButton'>Logout</button>
